dc01ed03a53ca5d85823d585e405d7fe0a835f65
= <97antjoh@gmail.com>
2018-08-22 23:11:01 +0200

    forgot header file for profiler

---
+++ include/weq/utility/Profiler.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <chrono>
+#include <string>
+#include <unordered_map>
+
+#define USE_PROFILING 1
+
+#if USE_PROFILING
+#define CONCAT_(a, b) a ## b
+#define CONCAT(a, b) CONCAT_(a, b)
+#define PROFILE(what) utility::Profiler CONCAT(__profiler__, __LINE__) (what);
+#else
+#define PROFILE(what) 
+#endif
+
+namespace utility{
+
+struct Profiler{
+	using Clock = std::chrono::high_resolution_clock;
+	struct time_entry{long long time; long long min; long long max;};
+
+	Profiler(const std::string& what) : what(what) {
+		start = Clock::now();
+	}
+
+	~Profiler(){
+		long long time = std::chrono::duration_cast<std::chrono::nanoseconds>(Clock::now() - start).count();
+		if(auto e = entries.find(what); e != entries.end()){
+			e->second.time += time;
+			e->second.time /= 2;
+			if(time < e->second.min)e->second.min = time;
+			if(time > e->second.max)e->second.max = time;
+		}else{
+			entries[what] = {time, time, time};
+		};
+	}
+
+	const std::string& what;
+	Clock::time_point start;
+	static std::unordered_map<std::string, time_entry> entries;
+};
+
+}
